286130f346e21c19593d6943fb415ea0cba9b29c
vaibhavpal02 <vaibhavpal2022@gmail.com>
2025-09-17 01:38:19 +0530

    Question 27 Added

---
+++ PracticeSet2/question27.java
@@ -0,0 +1,13 @@
+//Q27. Write a program to print the first 10 natural numbers in reverse order.
+
+public class question27
+{
+    public static void main(String[] args) 
+    {
+    for(int i=10;i>=1;i--)
+    {
+        System.out.print(i+" ");
+    }
+    }
+    
+}
