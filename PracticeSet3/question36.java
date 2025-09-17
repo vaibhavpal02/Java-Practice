9354d29e9f605c66985ea8a7930ead28955a9e8b
vaibhavpal02 <vaibhavpal2022@gmail.com>
2025-09-17 20:34:27 +0530

    Added question 36

---
+++ PracticeSet3/question36.java
@@ -0,0 +1,15 @@
+//Q36. Write a program to print the first N natural numbers in reverse order.
+import java.util.*;
+public class question36
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            System.out.print(i+" ");
+        }
+
+    }
+}
