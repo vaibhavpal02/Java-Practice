c829ce6fe97d6d08ef89b3aee93a097fb6706371
vaibhavpal02 <vaibhavpal2022@gmail.com>
2025-10-16 20:46:48 +0530

    Added Question 86

---
+++ PracticeSet7/question86.java
@@ -0,0 +1,20 @@
+//Right angled Triangle.
+import java.util.*;
+public class question86
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
